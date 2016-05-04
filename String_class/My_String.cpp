2b17465dc30b0f478f504916a5d8833bb43d27b4
embpgp <522943492@qq.com>
2016-05-04 14:45:38 +0000

    实现String类的四大基础函数

--- String_class/My_String.cpp
+++ String_class/My_String.cpp
@@ -3,12 +3,12 @@
 //普通构造函数
 My_String::My_String(const char *str)
 {
-	if(str == NULL)
+	if(str == NULL)   //说明发生默认构造
 	{
 		m_data = new char[1];
 		m_data = '\0';
 	}
-	else
+	else                      
 	{
 		int length = strlen(str);
 		m_data = new char[length + 1];
@@ -20,10 +20,11 @@ My_String::My_String(const char *str)
 //析构函数
 My_String::~My_String()
 {
-	delete []m_data;
+	delete []m_data;    //内置变量类型也可不加[]
 }
 
-//拷贝构造函数
+//拷贝构造函数 ,不可将other形参声明为值类型的，否则如果编译器允许
+//传值拷贝构造，将进行无限递归，造成程序崩溃，C++中尽量将对象参数声明为引用类型提高效率
 My_String::My_String(const My_String & other)
 {
 	int length = strlen(other.m_data);
@@ -40,7 +41,7 @@ My_String &My_String::operator=(const My_String &str)
 		return *this;
 	}
 	//刚刚看到剑指offer书上说如果此时程序先delete掉原有对象，而在new新的
-	//空间时法神bad_alloc等异常，将破坏掉原来的对象，因此，为了程序的健壮性
+	//空间时发生bad_alloc等异常，将破坏掉自身的对象，因此，为了程序的健壮性
 	//方法1，先进行new,成功后再delete原来的空间
 	//方法2，先创建临时实例，然后交换此时两者的指针，当临时的变量生命期结束时候调用析构函数自动传入
 	//赋值对象的指针，此时将被析构掉，而新的对象将继续存在，方法2如下：
@@ -52,6 +53,7 @@ My_String &My_String::operator=(const My_String &str)
 	
 }
 
+//简单地输出字符串而已
 void My_String::Print()
 {
 	cout<<m_data<<endl;
